57076f6c6cf1bec9e9b69ace6260b822369db6ea
Julien Dargelos <julien.dargelos@me.com>
2020-01-30 18:46:11 +0100

    Remove unfinished video support for now

--- src/video-ratio-texture.ts
+++
@@ -1,8 +0,0 @@
-import { mix } from 'ts-mixer'
-import { VideoTexture } from 'tree'
-import { ResponsiveTexture } from '~/responsive-texture'
-
-
-export @mix(VideoTexture, ResponsiveTexture) class VideoResponsiveTexture {
-
-}
